d27933264f1fb3700d9ea4b5f2a82fc680e5a373
Jarosław Szewczyk <jaroslaw.szewczyk@tutanota.com>
2020-01-26 10:48:10 +0000

    Finish @media

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+/* Hamburger */
+
+let hamburger = document.querySelector('.fas');
+let sidebar = document.querySelector('.sidebar');
+
+hamburger.addEventListener('click', function () {
+  sidebar.classList.toggle('activeSidebar');
+});
+
+
 /* Canvas */
 
 var ctx = document.getElementById('myChart').getContext('2d');
@@ -7,11 +17,11 @@ var chart = new Chart(ctx, {
   type: 'bar',
   data: {
     // 2
-    labels: ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10"],
+    labels: ['01', '02', '03', '04', '05', '06', '07', '08', '09', '10'],
     // 3
     datasets: [{
       // 4
-      label: "Signups",
+      label: 'Signups',
       // 5
       backgroundColor: '#8DBEC8',
       borderColor: '#8DBEC8',
@@ -19,13 +29,13 @@ var chart = new Chart(ctx, {
       data: [ 52, 51, 41, 94, 26, 6, 72, 9, 21, 88 ],
     },
     {
-      label: "FTD",
+      label: 'FTD',
       backgroundColor: '#F29E4E',
       borderColor: '#F29E4E',
       data: [ 6, 72, 1, 0, 47, 11, 50, 44, 63, 76 ],
     },
     {
-      label: "Earned",
+      label: 'Earned',
       backgroundColor: '#71B374',
       borderColor: '#71B374',
       data: [ 59, 49, 68, 90, 67, 41, 13, 38, 48, 48 ],
